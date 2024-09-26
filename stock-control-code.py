09b06fd1c637f155d350fb3c0003f063b03cf93a
José Luís Pereira Braga <66488939+jluispbraga@users.noreply.github.com>
2024-09-25 21:16:11 -0400

    Update stock-control-code.py

--- stock-control-code.py
+++ stock-control-code.py
@@ -1,5 +1,3 @@
-estoque_inicial = "Notebook Dell;201;15;3200.00;4500.00#Notebook Lenovo;202;10;2800.00;4200.00#Mouse Logitech;203;50;70.00;150.00#Mouse Razer;204;40;120.00;250.00#Monitor Samsung;205;0;800.00;1200.00#Monitor LG;206;0;750.00;1150.00#Teclado Mecânico Corsair;207;30;180.00;300.00#Teclado Mecânico Razer;208;25;200.00;350.00#Impressora HP;209;5;400.00;650.00#Impressora Epson;210;3;450.00;700.00#Monitor Dell;211;12;850.00;1250.00#Monitor AOC;212;7;700.00;1100.00"
-
 #Ao iniciar a sessão
 def introducao():
   '''
@@ -36,6 +34,8 @@ def cadastro_produto():
       Realiza o cadastramento de novos produtos no estoque. Pede detalhes como nome, código, quantidade, custo e preço.
       Retorna o estoque atualizado com o novo produto.
     '''
+    global estoque_inicial
+
     #Detalhamento do produto
     produtos = ""
     while(True):
@@ -249,6 +249,7 @@ def atualizar_estoque():
       Permite alterar a descrição, código, quantidade, custo e preço de venda do produto.
       O estoque é atualizado dinamicamente após a modificação.
     '''
+    global estoque_inicial
     produtos = estoque_inicial.split('#')
     lista_produtos = [produto.split(';') for produto in produtos]
     listagem_produtos()
@@ -377,7 +378,7 @@ def gerar_relatorio_estoque(produtos):
     print(f"Custo Total: {custo_total:.2f}".rjust(70))
     print(f"Faturamento Total: {faturamento_total:.2f}".rjust(70))
 
-
+estoque_inicial = "Notebook Dell;201;15;3200.00;4500.00#Notebook Lenovo;202;10;2800.00;4200.00#Mouse Logitech;203;50;70.00;150.00#Mouse Razer;204;40;120.00;250.00#Monitor Samsung;205;0;800.00;1200.00#Monitor LG;206;0;750.00;1150.00#Teclado Mecânico Corsair;207;30;180.00;300.00#Teclado Mecânico Razer;208;25;200.00;350.00#Impressora HP;209;5;400.00;650.00#Impressora Epson;210;3;450.00;700.00#Monitor Dell;211;12;850.00;1250.00#Monitor AOC;212;7;700.00;1100.00"
 
 #Introdução
 def main():
